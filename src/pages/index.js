a91e95ee69a69a9b48283a7f8ec95526f58ca466
Zerogami01 <zerogamimusic@gmail.com>
2020-12-03 02:31:06 +0900

    completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {graphql} from "gatsby"
+import {Link, graphql} from "gatsby"
 import {css} from "@emotion/react"
 import {rhythm} from "../utils/typography"
 import Layout from "../components/layout"
@@ -19,6 +19,13 @@ export default function Home({data}) {
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({node}) => (
           <div key={node.id}>
+            <Link
+              to={node.fields.slug}
+              css={css`
+                text-decoration: none;
+                color: inherit;
+              `}
+            >
             <h3
               css={css`
                 margin-bottom: ${rhythm(1/4)};
@@ -34,6 +41,7 @@ export default function Home({data}) {
               </span>
             </h3>
             <p>{node.excerpt}</p>
+            </Link>
           </div>
         ))}
       </div>
@@ -51,6 +59,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
